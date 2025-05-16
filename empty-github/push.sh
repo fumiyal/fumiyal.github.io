fbf3ce14ce45d904de5aeb2bde6881b2e41c70c8
John Doe <johndoe@example.com>
2025-05-16 22:28:21 +0900

    Initial; Created the app.

---
+++ empty-github/push.sh
@@ -0,0 +1,12 @@
+
+
+# cd "$(dirname "$0")"
+
+git add .
+git commit -m "Initial; Created the app."
+git branch -M main
+git remote add origin https://github.com/KusoGames/Kuso-Physics.git
+git push --force -u origin main
+
+# cd -
+
